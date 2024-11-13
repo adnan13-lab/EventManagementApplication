9bbde1685f5ed97a3ed5037a7ad34f102fe2dfc0
adnan13-lab <adnan.shah@systemsltd.com>
2024-11-13 19:54:53 +0500

    CategoryFile

---
+++ Event_Management_Application/Event_Management_Application_Models/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Management_Application_Models.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+
+        public string? name { get; set; }
+
+        public string? description { get; set; }
+    }
+}
